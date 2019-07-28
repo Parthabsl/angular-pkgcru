4c9a70b3de35e6a9ea11e6078db56e87df54a744
Parthabsl <partha.sarkar@birlasoft.com>
2019-07-28 21:19:21 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { HelloComponent } from './hello.component';
 import {HomeComponent} from './Home/home.component';
 import {ServerComponent} from './Server/server.component';
 import {UserComponent} from './User/user.component';
+import {Routes, RouterModule} from '@angular/router';
+
+const approutes: Routes = [
+  {path : '', component: HomeComponent},
+  {path : '/user', component: UserComponent},
+  {path : '/server', component: ServerComponent},
+  ];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(approutes) ],
   declarations: [ AppComponent, HelloComponent , HomeComponent, ServerComponent, UserComponent],
   bootstrap:    [ AppComponent ]
 })

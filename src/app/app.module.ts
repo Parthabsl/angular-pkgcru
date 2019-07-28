7844215494a1dbec967d876196ce4082e0f5362a
Parthabsl <partha.sarkar@birlasoft.com>
2019-07-28 21:08:34 +0530

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {HomeComponent} from './Home/home.component'
+import {HomeComponent} from './Home/home.component';
+import {ServerComponent} from './Server/server.component';
+import {UserComponent} from './User/user.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent , HomeComponent],
+  declarations: [ AppComponent, HelloComponent , HomeComponent, ServerComponent, UserComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
